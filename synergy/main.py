ebd107f2a7d7ed097c3f0beb00062233dfda772b
Marti Martinez <martinezah@gmail.com>
2020-07-25 08:19:13 +0000

    inbound function working

--- synergy/main.py
+++ synergy/main.py
@@ -1,3 +1,4 @@
+import aiohttp
 import aioredis
 import json
 import logging.config
@@ -7,7 +8,7 @@ import uuid
 
 from aiohttp_requests import requests
 from hashlib import sha256
-from quart import Quart, request, redirect
+from quart import Quart, request, redirect, make_response
 
 # setup
 import settings
@@ -17,37 +18,62 @@ app = Quart(__name__)
 # api
 @app.route('/go')
 async def go():
-    redis_pool = await aioredis.create_pool((settings.redis_host, settings.redis_port), db=settings.redis_db)
     cookies = {}
     for kk in settings.source_cookies:
         vv = request.cookies.get(kk) 
         if vv: cookies[kk] = vv
-    #app.logger.debug(f"cookies: {cookies}")
     resp = await requests.get(settings.source_auth_url, cookies=cookies, timeout=settings.source_timeout)
-    _data = await resp.text()
-    #app.logger.debug(f"resp: {resp.status} {_data}")
     if resp.status in settings.source_success_codes:
         key = request.args.get('_k')
         if key:
             nonce = str(uuid.uuid4())
-            ts = time.gmtime()
+            ts = time.time()
             data = json.dumps({
                 'nonce': nonce,
                 'key': key,
                 'ts': ts,
             })
             ticket = sha256(data.encode()).hexdigest()
-            await redis_pool.execute("setex", f"{settings.redis_prefix}{ticket}", settings.redis_timeout, data) 
+            redis_pool = await aioredis.create_pool((settings.redis_host, settings.redis_port), db=settings.redis_db)
+            await redis_pool.execute('setex', f'{settings.redis_prefix}{ticket}', settings.ticket_timeout, data) 
             redirect_url = settings.source_redirect_url.replace(settings.source_redirect_key, key)
-            return redirect(f"{redirect_url}?_t={ticket}")
-    app.logger.warn(json.dumps({"message": "Synergy auth error"}))
+            app.logger.info(json.dumps({'msg': 'Authentication success', 'redirect': redirect_url, 'ticket': ticket, 'ts': ts}))
+            return redirect(f'{redirect_url}?_t={ticket}')
+    app.logger.warn(json.dumps({'msg': 'Failed to authenticate', 'status': resp.status, 'cookies': cookies}))
     return ('', 403)
 
 @app.route('/chk')
 async def check():
-    # When called , grabs url param from request, looks up hash, if valid and in scope, logs int, extracts cookies, sends them to user, and redirects to a URL
-    redis_pool = await aioredis.create_pool((settings.redis_host, settings.redis_port), db=settings.redis_db)
-    return "check"
+    ts = time.time()
+    ticket = request.args.get('_t')
+    if ticket:
+        redis_pool = await aioredis.create_pool((settings.redis_host, settings.redis_port), db=settings.redis_db)
+        data = await redis_pool.execute('get', f'{settings.redis_prefix}{ticket}')
+        if data:
+            data = json.loads(data)
+            ticket_ts = data.get('ts')
+            key = data.get('key')
+            if ts - ticket_ts <= settings.ticket_timeout:
+                async with aiohttp.ClientSession() as session:
+                    headers = { 'kbn-xsrf': 'kibana' }
+                    login_url = settings.dest_auth_url.replace(settings.dest_auth_key, key)
+                    if settings.dest_auth_sub:
+                        login_url = login_url.replace(settings.dest_auth_sub, settings.dest_auth_with)
+                    auth_json = settings.dest_auth_creds.get(key, settings.dest_auth_creds.get('default'))
+                    resp = await session.post(login_url, json=auth_json, headers=headers)
+                    if resp.status in settings.dest_success_codes:
+                        response = await make_response(redirect(settings.dest_redirect_url))
+                        for kk in settings.dest_cookies:
+                            vv = resp.cookies.get(kk)
+                            if vv:
+                                response.set_cookie(kk, vv.value)
+                            else:
+                                app.logger.warn(json.dumps({'msg': 'Cookie not found', 'cookie': kk}))
+                        app.logger.info(json.dumps({'msg': 'Validation success', 'redirect': settings.dest_redirect_url}))
+                        return response
+    app.logger.warn(json.dumps({'msg': 'Failed to validate login', 'ticket': ticket, 'ts': ts, 'ticket_data': data}))
+    return redirect(settings.dest_login_url)
+
     
 # for dev use only    
 if __name__ == '__main__': 
